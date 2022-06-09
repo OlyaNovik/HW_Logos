848e4ccda394dc419b920d1462671e04e9362433
OlyaNovik <olyaanovikk@gmail.com>
2022-06-09 11:56:07 +0300

    Add last homework for event(keyboard and background color/image)

---
+++ JavaScript/HW_color.image/script-color-pic.js
@@ -0,0 +1,51 @@
+let h1 = document.querySelector('.color');
+let wrBlocks = document.querySelector('.colorBlocks')
+h1.addEventListener('click',()=>{
+    wrBlocks.style.display = 'grid';
+    wrPictures.style.display = 'none';
+
+})
+
+
+let h2 = document.querySelector('.picture');
+let wrPictures = document.querySelector('.pictures')
+h2.addEventListener('click',()=>{
+    wrPictures.style.display = 'grid';
+    wrBlocks.style.display = 'none';
+
+})
+
+let blocks2 = document.getElementsByClassName('block2');
+let BG = [
+    'https://images.unsplash.com/photo-1472214103451-9374bd1c798e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGljfGVufDB8fDB8fA%3D%3D&w=1000&q=80',
+    'https://images.unsplash.com/photo-1527960669566-f882ba85a4c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YXdlc29tZSUyMHBpY3xlbnwwfHwwfHw%3D&w=1000&q=80',
+    'https://www.myfitness.ee/wp-content/uploads/2016/05/nature-wallpaper-07.jpg',
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/4/42/Shaqi_jrvej.jpg/1200px-Shaqi_jrvej.jpg',
+    'https://sferaglass.com/storage/images/ZCyNe6QvEf.jpeg',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJrsnfxCeHArU4QxIxYLVcx69EjchHCEtmUQ&usqp=CAU',
+    'https://production-wordpress.unep-wcmc.org/content//uploads/2021/11/AdobeStock_291451528-1-scaled.jpeg',
+    'https://cf.bstatic.com/xdata/images/hotel/max1024x768/279314888.jpg?k=634792d1c74f7c16f71502c6e4f2dd83e56c5745965ea94c3205046ca4736217&o=&hp=1',
+    'https://cdn.futura-sciences.com/buildsv6/images/wide1920/1/f/3/1f3a0c73fe_50143651_496.jpg',
+]
+
+for (let i=0;i<=blocks2.length;i++){
+    blocks2[i]?.addEventListener('click', ()=>{
+        document.body.style.backgroundImage = 'url(' + BG[i] + ')';
+    })
+}
+
+
+let blocks1 = document.getElementsByClassName('block');
+let color = ['aqua','brown','green','lightgreen','gray','red','blue','blueviolet','olive']
+
+// for (let i=0;i<=blocks1.length;i++){
+//     blocks1[i].addEventListener('click', ()=>{
+//         document.body.style.background = color[i];
+//     })
+// }
+
+for (let i=0;i<=blocks1.length;i++){
+    blocks1[i]?.addEventListener('click', ()=>{
+        document.body.style.background = color[i];
+    })
+}
---
+++ JavaScript/HWkeyboard/keyboard-js.js
@@ -0,0 +1,131 @@
+
+
+// input.addEventListener('keydown', (event)=>{
+
+// switch (event.keyCode){
+//     case 8: backspace.style.background = 'gray', backspace.style.color = 'white';
+//     break;
+//     case 32: space.style.background = 'gray';
+//     break;
+//     case 9: TAB.style.background = 'gray', TAB.style.color = 'white';
+//     break;
+//     case 20: CAPS.style.background = 'gray', CAPS.style.color = 'white';
+//     break;
+//     case 13: ENTER.style.background = 'gray', ENTER.style.color = 'white';
+//     break;
+//     case 16: SHIFT1.style.background = 'gray', SHIFT1.style.color = 'white', SHIFT2.style.background = 'gray', SHIFT2.style.color = 'white';
+//     break;
+//     case 48: zero.style.background = 'gray', zero.style.color = 'white';
+//     break;
+//     case 49: one.style.background = 'gray', one.style.color = 'white';
+//     break;
+//     case 50: two.style.background = 'gray', two.style.color = 'white';
+//     break;
+//     case 51: tree.style.background = 'gray', tree.style.color = 'white';
+//     break;
+//     case 52: four.style.background = 'gray', four.style.color = 'white';
+//     break;
+//     case 53: five.style.background = 'gray', five.style.color = 'white';
+//     break;
+//     case 54: six.style.background = 'gray', six.style.color = 'white';
+//     break;
+//     case 55: seven.style.background = 'gray', seven.style.color = 'white';
+//     break;
+//     case 56: eight.style.background = 'gray', eight.style.color = 'white';
+//     break;
+//     case 57: nine.style.background = 'gray', nine.style.color = 'white';
+//     break;  
+//     case 192: tilda.style.background = 'gray', tilda.style.color = 'white';
+//     break;
+//     case 189: minus.style.background = 'gray', minus.style.color = 'white';
+//     break;  
+//     case 187: equel.style.background = 'gray', equel.style.color = 'white';
+//     break;    
+// }
+// // console.log(event.keyCode);
+// let i = event.keyCode;
+// console.log(i);
+
+
+// input.addEventListener('keyup', (event)=>{
+
+//     switch (event.keyCode){
+//         case 8: backspace.style.background = 'white', backspace.style.color = 'black';
+//         break;
+//         case 32: space.style.background = 'white';
+//         break;
+//         case 9: TAB.style.background = 'white', TAB.style.color = 'black';
+//         break;
+//         case 20: CAPS.style.background = 'white', CAPS.style.color = 'black';
+//         break;
+//         case 13: ENTER.style.background = 'white', ENTER.style.color = 'black';
+//         break;
+//         case 16: SHIFT1.style.background = 'white', SHIFT1.style.color = 'black', SHIFT2.style.background = 'white', SHIFT2.style.color = 'black';
+//         break;
+//         case 48: zero.style.background = 'white', zero.style.color = 'black';
+//         break;
+//         case 49: one.style.background = 'white', one.style.color = 'black';
+//         break;
+//         case 50: two.style.background = 'white', two.style.color = 'black';
+//         break;
+//         case 51: tree.style.background = 'white', tree.style.color = 'black';
+//         break;
+//         case 52: four.style.background = 'white', four.style.color = 'black';
+//         break;
+//         case 53: five.style.background = 'white', five.style.color = 'black';
+//         break;
+//         case 54: six.style.background = 'white', six.style.color = 'black';
+//         break;
+//         case 55: seven.style.background = 'white', seven.style.color = 'black';
+//         break;
+//         case 56: eight.style.background = 'white', eight.style.color = 'black';
+//         break;
+//         case 57: nine.style.background = 'white', nine.style.color = 'black';
+//         break; 
+//         case 192: tilda.style.background = 'white', tilda.style.color = 'black';
+//         break; 
+//         case 189: minus.style.background = 'white', minus.style.color = 'black';
+//         break;  
+//         case 187: equel.style.background = 'white', equel.style.color = 'black';
+//         break;    
+//     }
+//     })
+
+
+
+
+
+let input = document.querySelector('.keyInput');
+
+let numbers = document.getElementsByTagName('div');
+
+let codeOfNumbers=[192,49,50,51,52,53,54,55,56,57,48,189,187,8,9,81,87,69,82,84,89,85,73,79,80,219,221,220,20,65,83,68,70,71,72,74,75,76,186,222,13,16,90,88,67,86,66,78,77,188,190,191,16,32];
+
+
+input.addEventListener('keydown', (event)=>{
+    let code=event.keyCode;
+   for(let i=0;i<codeOfNumbers.length;i++){
+    if(code==codeOfNumbers[i]){
+        numbers[i].style.background='gray';
+       console.log(codeOfNumbers[i]);}
+   }
+   
+    
+}) 
+
+input.addEventListener('keyup', (event)=>{
+    let code=event.keyCode;
+   for(let i=0;i<codeOfNumbers.length;i++){
+    if(code==codeOfNumbers[i]){
+        numbers[i].style.background='white';
+       console.log(codeOfNumbers[i]);}
+   }
+   
+    
+})
+
+input.addEventListener('keydown', (event)=>{
+    let i = event.keyCode;
+    console.log(i);
+})
+
